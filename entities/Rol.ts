fc5dc66790ba764827adbd3f8abeace7f1363d08
Jairo Vanegas <jhvangar@gmail.com>
2020-12-12 18:12:12 -0500

    Clases iniciales

---
+++ entities/Rol.ts
@@ -0,0 +1,5 @@
+export default interface Rol{
+    identificador: string,
+    descripcion: string,
+    permisos: number
+}
---
+++ entities/Usuario.ts
@@ -0,0 +1,10 @@
+export default interface Usuario{
+    identificacion: string,
+    nombres: string,
+    apellidos: string,
+    password: string,
+    cambiar_password: boolean,
+    no_informacion: boolean,
+    roles:[],
+    firma: string
+}
