977265a895b7414c47a65b7fccfb33f4e1f8b27e
Jairo Vanegas <jhvangar@gmail.com>
2021-01-16 18:26:15 -0500

    Enumerador de errores.

--- dist/errors/index.js
+++ dist/errors/index.js
@@ -10,5 +10,15 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.ErrorEnum = void 0;
 __exportStar(require("./Errores"), exports);
+var ErrorEnum;
+(function (ErrorEnum) {
+    ErrorEnum[ErrorEnum["USUARIO_NO_ENCONTRADO"] = 0] = "USUARIO_NO_ENCONTRADO";
+    ErrorEnum[ErrorEnum["CREDENCIALES_INCORRECTAS"] = 1] = "CREDENCIALES_INCORRECTAS";
+    ErrorEnum[ErrorEnum["CAMBIO_PASS"] = 2] = "CAMBIO_PASS";
+    ErrorEnum[ErrorEnum["FALTA_INFORMACION"] = 3] = "FALTA_INFORMACION";
+    ErrorEnum[ErrorEnum["SIN_AUTORIZACION"] = 4] = "SIN_AUTORIZACION";
+    ErrorEnum[ErrorEnum["TOKEN_INVALIDO"] = 5] = "TOKEN_INVALIDO";
+})(ErrorEnum = exports.ErrorEnum || (exports.ErrorEnum = {}));
 //# sourceMappingURL=index.js.map
--- errors/index.ts
+++ errors/index.ts
@@ -1,1 +1,9 @@
 export * from './Errores';
+export enum ErrorEnum {
+    "USUARIO_NO_ENCONTRADO",
+    "CREDENCIALES_INCORRECTAS",
+    "CAMBIO_PASS",
+    "FALTA_INFORMACION",
+    "SIN_AUTORIZACION",
+    "TOKEN_INVALIDO"
+}
