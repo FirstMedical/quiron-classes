22db84970be2e44824db03096ce060ecf9aca75c
Jairo Vanegas <jhvangar@gmail.com>
2021-05-08 23:22:59 -0500

    Actualizado el uuid

--- entities/Plantilla.ts
+++ entities/Plantilla.ts
@@ -1,7 +1,7 @@
 import {CampoSeccion} from "../components/campos/edicion/elementos";
 import {Type} from "class-transformer";
 import Cacheable from "./auxiliar/Cacheable";
-import {uuid} from "uuidv4";
+import {v4} from "uuid";
 
 export default class Plantilla extends Cacheable{
     identificador: string = "";
@@ -15,7 +15,7 @@ export default class Plantilla extends Cacheable{
     constructor(identificador?: string, serial?: string, version?: number, modelo?: CampoSeccion, relacionadas?: Plantilla[]) {
         super();
         this.identificador = identificador? identificador : "";
-        this.serial = serial? serial : uuid();
+        this.serial = serial? serial : v4();
         this.version = version? version : 1;
         this.modelo = modelo? modelo : new CampoSeccion("raiz");
         this.relacionadas = relacionadas? relacionadas : [];
