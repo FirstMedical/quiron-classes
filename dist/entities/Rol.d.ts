c0754bbe34cf7cd9ce2f983100b727679eb13288
Jairo Vanegas <jhvangar@gmail.com>
2021-07-22 16:25:24 -0500

    Correccion de rol por metodos

--- dist/entities/Rol.d.ts
+++ dist/entities/Rol.d.ts
@@ -3,8 +3,8 @@ import Usuario from "./Usuario";
 export default class Rol extends DBEntity {
     identificador: string;
     descripcion: string;
-    permisos: Record<string, string>;
+    permisos: Record<string, Record<string, string[]>>;
     usuarios: Usuario[];
-    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, string>, usuarios?: Usuario[]);
+    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, Record<string, string[]>>, usuarios?: Usuario[]);
 }
 //# sourceMappingURL=Rol.d.ts.map
--- entities/Rol.ts
+++ entities/Rol.ts
@@ -4,11 +4,11 @@ import Usuario from "./Usuario";
 export default class Rol extends DBEntity {
     identificador: string = "";
     descripcion: string = "";
-    permisos: Record<string, string> = {};
+    permisos: Record<string, Record<string, string[]>> = {};
     usuarios: Usuario[];
 
 
-    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, string>, usuarios?: Usuario[]) {
+    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, Record<string, string[]>>, usuarios?: Usuario[]) {
         super();
         this.identificador = identificador ? identificador : "";
         this.descripcion = descripcion ? descripcion : "";
