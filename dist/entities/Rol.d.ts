668506d8a6e48dd67ad2b3e8dad0de5d238dc1cd
Jairo Vanegas <jhvangar@gmail.com>
2021-07-21 23:46:59 -0500

    Correcion rol permisos

--- dist/entities/Rol.d.ts
+++ dist/entities/Rol.d.ts
@@ -3,8 +3,8 @@ import Usuario from "./Usuario";
 export default class Rol extends DBEntity {
     identificador: string;
     descripcion: string;
-    permisos: Record<string, boolean>;
+    permisos: Record<string, string>;
     usuarios: Usuario[];
-    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, boolean>, usuarios?: Usuario[]);
+    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, string>, usuarios?: Usuario[]);
 }
 //# sourceMappingURL=Rol.d.ts.map
--- entities/Rol.ts
+++ entities/Rol.ts
@@ -4,11 +4,11 @@ import Usuario from "./Usuario";
 export default class Rol extends DBEntity {
     identificador: string = "";
     descripcion: string = "";
-    permisos: Record<string, boolean> = {};
+    permisos: Record<string, string> = {};
     usuarios: Usuario[];
 
 
-    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, boolean>, usuarios?: Usuario[]) {
+    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, string>, usuarios?: Usuario[]) {
         super();
         this.identificador = identificador ? identificador : "";
         this.descripcion = descripcion ? descripcion : "";
