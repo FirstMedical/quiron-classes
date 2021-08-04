7ae037f8fd90d352d7d4cb3e9107bfb0fbf86c53
Jairo Vanegas <jhvangar@gmail.com>
2021-08-04 14:40:18 -0500

    Rol actualizado

--- dist/entities/Rol.d.ts
+++ dist/entities/Rol.d.ts
@@ -1,10 +1,11 @@
 import DBEntity from "./auxiliar/DBEntity";
 import Usuario from "./Usuario";
+import { Permission } from "../auth";
 export default class Rol extends DBEntity {
     identificador: string;
     descripcion: string;
-    permisos: Record<string, Record<string, string[]>>;
+    permisos: Record<string, Record<string, Permission[]>>;
     usuarios: Usuario[];
-    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, Record<string, string[]>>, usuarios?: Usuario[]);
+    constructor(identificador?: string, descripcion?: string, permisos?: Record<string, Record<string, Permission[]>>, usuarios?: Usuario[]);
 }
 //# sourceMappingURL=Rol.d.ts.map
